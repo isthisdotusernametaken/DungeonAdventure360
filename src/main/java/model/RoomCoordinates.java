b1f1b51b35e6acb465b4e69c2323d7d8203d8b3d
Joshua Barbee <joshuaa.bbarbee@gmail.com>
2022-11-01 20:15:36 -0700

    Added RoomCoordinates class for an int triple indicating the dimensions of or a position in a dungeon

---
+++ src/main/java/model/RoomCoordinates.java
@@ -0,0 +1,27 @@
+package model;
+
+public class RoomCoordinates {
+    private final int myFloor;
+    private final int myX;
+    private final int myY;
+
+    RoomCoordinates(final int theFloor,
+                    final int theX,
+                    final int theY) {
+        myFloor = theFloor;
+        myX = theX;
+        myY = theY;
+    }
+
+    int getFloor() {
+        return myFloor;
+    }
+
+    int getX() {
+        return myX;
+    }
+
+    int getY() {
+        return myY;
+    }
+}
