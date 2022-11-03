3e9c79e1fb9efa2c718407852c4dffb368d8779a
Tinh Diep <tdiep00@uw.edu>
2022-11-03 13:22:18 -0700

    Done creating MapView.java with classes and stubs

---
+++ src/main/java/view/MapView.java
@@ -0,0 +1,21 @@
+package view;
+
+import controller.Controller;
+
+import javax.naming.ldap.Control;
+
+public class MapView {
+    private static final String PROMPT = null;
+
+    void open(Controller theController) {
+
+    }
+
+    private void displayMap(Controller theController) {
+
+    }
+
+    private void waitForExit() {
+
+    }
+}
