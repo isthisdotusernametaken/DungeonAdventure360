2b95232baa7a708fd333af4d95b74497708ec1b3
kokinh11 <115815767+kokinh11@users.noreply.github.com>
2022-11-03 13:41:44 -0700

    Done creating InventoryView.java with classes and stubs

---
+++ src/main/java/view/InventoryView.java
@@ -0,0 +1,28 @@
+package view;
+
+import controller.Controller;
+
+public class InventoryView {
+    private static final String SELECT_PROMPT = null;
+    private static final Menu ITEM_ACTION_MENU = null;
+
+    void open(Controller theController) {
+
+    }
+
+    private void displayItems(Controller theController) {
+
+    }
+
+    private int selectItem(int theMaxIndex) {
+        return 0;
+    }
+
+    private boolean attemptUseItem(Controller theController, int theIndex) {
+        return false;
+    }
+
+    private void dropItem(Controller theController, int theIndex) {
+
+    }
+}
