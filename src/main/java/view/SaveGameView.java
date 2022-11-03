bc2a4b9e7bd457c5eda55de35c00f3de1467813f
Tinh Diep <tdiep00@uw.edu>
2022-11-03 13:23:13 -0700

    Done creating SaveGameView.java with classes and stubs

---
+++ src/main/java/view/SaveGameView.java
@@ -0,0 +1,19 @@
+package view;
+
+import controller.Controller;
+
+public class SaveGameView {
+    private static final Menu CONFIRM_OVERWRITE_MENU = null;
+
+    private void displayFiles(Controller theController) {
+
+    }
+
+    private String readFileName() {
+        return "";
+    }
+
+    private String attemptSaveGame(Controller theController) {
+        return "";
+    }
+}
