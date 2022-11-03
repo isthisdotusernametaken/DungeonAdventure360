7afe1ad46307e93084a198f8dc8a60124d970167
kokinh11 <115815767+kokinh11@users.noreply.github.com>
2022-11-03 13:42:31 -0700

    Done creating InputReader.java with classes and stubs

---
+++ src/main/java/view/InputReader.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputReader {
+    private static final Scanner CONSOLE_INPUT = null;
+    private static final String EXIT_MENU = null;
+
+    String readLine() {
+        return "";
+    }
+
+    boolean exitSent(String theUserInput) {
+        return false;
+    }
+}
