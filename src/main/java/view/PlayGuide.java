77c2c7f4eb37604ce69153c69f8cf2b5a2a81350
Tinh Diep <tdiep00@uw.edu>
2022-11-03 13:23:00 -0700

    Done creating PlayGuide.java with classes and stubs

---
+++ src/main/java/view/PlayGuide.java
@@ -0,0 +1,29 @@
+package view;
+
+public class PlayGuide {
+    private static final Menu GUIDE_MENU = null;
+
+    void open() {
+
+    }
+
+    private String printControlGuide() {
+        return "";
+    }
+
+    private String printCharactersGuide() {
+        return "";
+    }
+
+    private String printInventoryGuide() {
+        return "";
+    }
+
+    private String printExplorationGuide() {
+        return "";
+    }
+
+    private String printCombatGuide() {
+        return "";
+    }
+}
