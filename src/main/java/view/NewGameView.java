d8b90edb0887ea0a3549db80d7cc0cddf620cf16
Tinh Diep <tdiep00@uw.edu>
2022-11-03 13:22:51 -0700

    Done creating NewGameView.java with classes and stubs

---
+++ src/main/java/view/NewGameView.java
@@ -0,0 +1,36 @@
+package view;
+
+import controller.Controller;
+
+public class NewGameView {
+    private static final Menu CLASS_MENU = null;
+    private static final Menu DIFFICULTY_MENU = null;
+
+    MenuSignal open(Controller theController) {
+        return null;
+    }
+
+    private void displayFiles(Controller theController) {
+
+    }
+
+    private String readFileName() {
+        return "";
+    }
+
+    private String readAdventurerName() {
+        return "";
+    }
+
+    private String readAdventurerClass() {
+        return "";
+    }
+
+    private String readDifficulty() {
+        return "";
+    }
+
+    private String attemptCreateGame(Controller theController) {
+        return "";
+    }
+}
