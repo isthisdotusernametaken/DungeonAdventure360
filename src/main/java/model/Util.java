4a0dabc046a0d98a2089fe20d43be4feb9ef8eb4
Joshua Barbee <joshuaa.bbarbee@gmail.com>
2022-11-04 15:59:20 -0700

    Partial merge from barbeej (Util.java)

---
+++ src/main/java/model/Util.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.SplittableRandom;
+
+public class Util {
+
+    private static final SplittableRandom RANDOM = new SplittableRandom();
+
+    static boolean probabilityTest(final double theProbability) {
+        return randomDouble() < theProbability;
+    }
+
+    static double randomDouble() {
+        return RANDOM.nextDouble(1.0);
+    }
+
+    static int randomInt(final int theMin, final int theMax) {
+        return RANDOM.nextInt(theMin, theMax + 1);
+    }
+
+    static double clampFraction(final double theFraction) {
+        return Math.max(
+                0.0,
+                Math.min(theFraction, 1.0)
+        );
+    }
+
+    static int clampSpeed(final int theSpeed) {
+        return Math.max(1, theSpeed);
+    }
+}
