fe6bbfd3f77b1cddb0184da4c6555d9e95de305c
Tinh Diep <tdiep00@uw.edu>
2022-11-06 00:40:49 -0700

    Added Special Skills

---
+++ src/main/java/model/Heal.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Heal implements SpecialSkill{
+
+    private final static int MIN_HEAL = 0;
+    private final static int MAX_HEAL = 0;
+
+    @Override
+    public AttackResult use(DungeonCharacter theTarget) {
+        return null;
+    }
+}
---
+++ src/main/java/model/SneakAttack.java
@@ -0,0 +1,12 @@
+package model;
+
+public class SneakAttack implements SpecialSkill{
+
+    private final static double SUCCESS_CHANCE = 0;
+    private final static double NORMAL_ATTACK_CHANCE = 0;
+
+    @Override
+    public AttackResult use(DungeonCharacter theTarget) {
+        return null;
+    }
+}
