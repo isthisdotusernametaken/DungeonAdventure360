2c3ad13b5ac59ab7a7d86f7ed5f9ed9b367044fd
Joshua Barbee <joshuaa.bbarbee@gmail.com>
2022-11-15 21:30:45 -0800

    Completed maze generation in Dungeon Replaced Pillar constant instances with factory method to call once when generating Dungeon (to avoid Singleton deserialization issues)

--- src/main/java/model/Pillar.java
+++ src/main/java/model/Pillar.java
@@ -2,10 +2,10 @@ package model;
 
 public class Pillar extends Item {
 
-    final static Pillar ABSTRACTION = new Pillar(ItemType.ABSTRACTION, 'A');
-    final static Pillar ENCAPSULATION = new Pillar(ItemType.ENCAPSULATION, 'E');
-    final static Pillar INHERITANCE = new Pillar(ItemType.INHERITANCE, 'I');
-    final static Pillar POLYMORPHISM = new Pillar(ItemType.POLYMORPHISM, 'P');
+    private static final char ABSTRACTION_REPRESENTATION = 'A';
+    private static final char ENCAPSULATION_REPRESENTATION = 'E';
+    private static final char INHERITANCE_REPRESENTATION = 'I';
+    private static final char POLYMORPHISM_REPRESENTATION = 'P';
 
     private Pillar(final ItemType theType,
                    final char theCharRepresentation) {
@@ -15,6 +15,15 @@ public class Pillar extends Item {
                 false,
                 1
         );
+    }
+
+    static Pillar[] createPillars() {
+        return new Pillar[]{
+                new Pillar(ItemType.ABSTRACTION, ABSTRACTION_REPRESENTATION),
+                new Pillar(ItemType.ENCAPSULATION, ENCAPSULATION_REPRESENTATION),
+                new Pillar(ItemType.INHERITANCE, INHERITANCE_REPRESENTATION),
+                new Pillar(ItemType.POLYMORPHISM, POLYMORPHISM_REPRESENTATION)
+        };
     }
 
     @Override
