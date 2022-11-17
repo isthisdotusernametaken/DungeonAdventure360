198f02a372ebbaf0220784108eb03039be2e5275
Joshua Barbee <joshuaa.bbarbee@gmail.com>
2022-11-17 12:33:28 -0800

    Created empty test classes for factories

---
+++ src/test/java/model/MockDBManager.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.sql.ResultSet;
+
+public class MockDBManager {
+
+    static ResultSet readTable(final String theTable) {
+        return null;
+    }
+}
---
+++ src/test/java/model/TemplateGeneratorTest.java
@@ -0,0 +1,11 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+public class TemplateGeneratorTest {
+
+    @Test
+    void testConstructor() {
+
+    }
+}
