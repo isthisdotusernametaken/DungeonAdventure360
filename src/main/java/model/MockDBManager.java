bf978c66ebc09864d75a32d07ae354eee6edfdf3
Joshua Barbee <joshuaa.bbarbee@gmail.com>
2022-11-17 14:16:01 -0800

    Added default table values for MockDBManager

--- src/main/java/model/MockDBManager.java
+++ src/main/java/model/MockDBManager.java
@@ -4,7 +4,33 @@ import java.util.HashMap;
 
 public class MockDBManager {
 
+    private static final String[][] MONSTERS = {
+            {
+                "TestMonster", "400", "20", "30", "0.5", "0.05", "2", "BLUNT",
+                "5", "0.05", "0.2", "0.1 0.0 0.2 0.5 0.1"
+            }
+    };
+    private static final String[][] ADVENTURERS = {
+            {
+                    "TestAdventurer", "800", "60", "80", "0.8", "0.15", "3",
+                    "SHARP", "12", "0.1", "0.1 0.1 0.1 0.0 0.0"
+            }
+    };
+    private static final String[][] TRAPS = {
+            {
+                    "TestTrap", "false", "true", "50", "100", "0.8", "0.5",
+                    "4", "BLUNT", "5", "P"
+            }
+    };
+
     private static final HashMap<String, String[][]> TABLES = new HashMap<>();
+
+
+    static {
+        setTable("Monsters", MONSTERS);
+        setTable("Adventurers", ADVENTURERS);
+        setTable("Traps", TRAPS);
+    }
 
     static Table readTable(final String theTable) {
         return new MockTable(TABLES.get(theTable));
