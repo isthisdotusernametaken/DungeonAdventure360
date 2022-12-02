2fde1b42c58f7773a51ee43ad428912077b1bc9c
Joshua Barbee <joshuaa.bbarbee@gmail.com>
2022-12-02 10:18:10 -0800

    Started ExplorationView

--- src/main/java/view/ExplorationView.java
+++ src/main/java/view/ExplorationView.java
@@ -1,15 +1,32 @@
 package view;
 
 import controller.Controller;
+import model.Direction;
+
+import java.util.Arrays;
 
 public class ExplorationView {
 
-    private static final Menu ROOM_MENU = null;
-    private static final Menu MOVE_MENU = null;
+    private static final Menu ROOM_MENU = new Menu(
+            "Current room:",
+            new String[]{"Move", "Open Inventory", "Open Map", "Save", "Load", "Quit to Title Screen"},
+            false
+    );
+    private static final Menu MOVE_MENU = new Menu(
+            "Choose a direction to move",
+            Arrays.stream(Direction.values())
+                  .map(Direction::toString)
+                  .toArray(String[]::new),
+            Arrays.stream(Direction.values())
+                  .map(dir -> dir.toString().substring(0, 1))
+                  .toArray(String[]::new),
+            true
+    );
 
     static MenuSignal open(Controller theController) {
         return null;
     }
+
 
     private static void displayRoom(Controller theController) {
 
