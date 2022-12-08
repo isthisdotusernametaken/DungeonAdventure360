485a910c9693828bc0750057ff6c128b13b9b2e8
Tinh Diep <tdiep00@uw.edu>
2022-12-08 00:23:34 -0800

    Created tests for BuffTest Created tests for AdjustedCharacterStatsTest

--- src/test/java/model/AdjustedCharacterStatsTest.java
+++ src/test/java/model/AdjustedCharacterStatsTest.java
@@ -1,4 +1,105 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AdjustedCharacterStatsTest {
+
+    private AdjustedCharacterStats stats = new AdjustedCharacterStats(
+            new Monster(
+                    "Skeleton",
+                    "Skeleton",
+                    110,
+                    15,
+                    25,
+                    0.5,
+                    0.1,
+                    2,
+                    DamageType.SHARP,
+                    4,
+                    0.05,
+                    0.3,
+                    new ResistanceData(new double[]{0.1, 0.1, 0.0, 0.2, 0.2})));
+
+    @Test
+    void getMinDamageTest () {
+        assertEquals(15, stats.getMinDamage());
+    }
+
+    @Test
+    void getMaxDamageTest () {
+        assertEquals(25, stats.getMaxDamage());
+    }
+
+    @Test
+    void getHitChanceTest () {
+        assertEquals(0.5, stats.getHitChance());
+    }
+
+    @Test
+    void getSpeedTest () {
+        assertEquals(4, stats.getSpeed());
+    }
+
+    @Test
+    void getResistanceTest () {
+        assertEquals(0.1, stats.getResistance(DamageType.NORMAL));
+        assertEquals(0.1, stats.getResistance(DamageType.SHARP));
+        assertEquals(0.0, stats.getResistance(DamageType.BLUNT));
+        assertEquals(0.2, stats.getResistance(DamageType.FIRE));
+        assertEquals(0.2, stats.getResistance(DamageType.POISON));
+    }
+
+    @Test
+    void multiplyDamageTest () {
+        stats.multiplyDamage(2);
+        assertEquals(30, stats.getMinDamage());
+        assertEquals(50, stats.getMaxDamage());
+    }
+
+    @Test
+    void multiplyHitChanceTest () {
+        stats.multiplyHitChance(200000);
+        assertEquals(1.0, stats.getHitChance());
+    }
+
+    @Test
+    void multiplySpeedTest () {
+        stats.multiplySpeed(9999);
+        assertEquals(39996, stats.getSpeed());
+    }
+
+    @Test
+    void multiplyResistancesTest () {
+        stats.multiplyResistances(99999999);
+        assertEquals(1.0, stats.getResistance(DamageType.NORMAL));
+        assertEquals(1.0, stats.getResistance(DamageType.SHARP));
+        assertEquals(0.0, stats.getResistance(DamageType.BLUNT));
+        assertEquals(1.0, stats.getResistance(DamageType.FIRE));
+        assertEquals(1.0, stats.getResistance(DamageType.POISON));
+    }
+
+    @Test
+    void resetStatsTest () {
+        stats.multiplyDamage(2);
+        stats.multiplyHitChance(200000);
+        stats.multiplySpeed(9999);
+        stats.resetStats();
+        assertEquals(15, stats.getMinDamage());
+        assertEquals(25, stats.getMaxDamage());
+        assertEquals(0.5, stats.getHitChance());
+        assertEquals(4, stats.getSpeed());
+    }
+
+    @Test
+    void resetResistancesTest () {
+        stats.multiplyResistances(2);
+        stats.resetResistances();
+        assertEquals(0.1, stats.getResistance(DamageType.NORMAL));
+        assertEquals(0.1, stats.getResistance(DamageType.SHARP));
+        assertEquals(0.0, stats.getResistance(DamageType.BLUNT));
+        assertEquals(0.2, stats.getResistance(DamageType.FIRE));
+        assertEquals(0.2, stats.getResistance(DamageType.POISON));
+    }
 }
--- src/test/java/model/BuffTest.java
+++ src/test/java/model/BuffTest.java
@@ -1,4 +1,61 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
 public class BuffTest {
+    private StrengthBuff myBuff;
+
+    @BeforeEach
+    void setUpBuff() {
+        myBuff = new StrengthBuff(1);
+    }
+
+    @Test
+    void testGetBuffType() {
+        assertNotEquals(BuffType.BROKEN_BONE, myBuff.getType());
+        assertEquals(BuffType.STRENGTH, myBuff.getType());
+    }
+
+    @Test
+    void testGetStatMultiplier() {
+        assertNotEquals(154, myBuff.getStatMultiplier());
+        assertEquals(1.5, myBuff.getStatMultiplier());
+    }
+
+    @Test
+    void testGetDamagePercent() {
+        assertNotEquals(10.0, myBuff.getDamagePercent());
+        assertEquals(0.0, myBuff.getDamagePercent());
+    }
+
+    @Test
+    void testGetDuration() {
+        assertNotEquals(2, myBuff.getDuration());
+        assertEquals(1, myBuff.getDuration());
+    }
+
+    @Test
+    void testChangeDuration() {
+        myBuff.changeDuration(4);
+        assertNotEquals(4, myBuff.getDuration());
+        assertEquals(5, myBuff.getDuration());
+    }
+
+    @Test
+    void testAdvanceBuffDuration() {
+        myBuff.advance();
+        assertEquals(0, myBuff.getDuration());
+    }
+
+    @Test
+    void testCheckIsCompleted() {
+        myBuff.advance();
+        assertNotEquals(false, myBuff.isCompleted());
+        assertEquals(true, myBuff.isCompleted());
+    }
 }
